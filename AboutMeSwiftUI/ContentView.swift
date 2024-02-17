bbffc7ca8200b214135c16bf72c0039c3e41add9
Takogero <149505416+Takogero@users.noreply.github.com>
2024-02-17 15:58:58 +0300

    Update ContentView.swift

--- AboutMeSwiftUI/ContentView.swift
+++ AboutMeSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
             Spacer()
-            Text("IOS разработчик - Привет! Я начинающий IOS разработчик. Написал несколько проектов с помощью языка Swift на UIKit, взаимодействовал с API с помощью URLSession и JSONDecoder.  Для контроля версий использовал Git.  Верстал кодом с помощью ProgrammaticUI и SnapKit.  В качестве архитектуры проекта выбирал MVC и MVP.  Для базы данных использовал Realm и Core Data.  Подключал зависимости через Swift Package Manager и CocoaPods")
+            Text("IOS разработчик - Привет. Я начинающий IOS разработчик. Написал несколько проектов с помощью языка Swift на UIKit, взаимодействовал с API с помощью URLSession и JSONDecoder.  Для контроля версий использовал Git.  Верстал кодом с помощью ProgrammaticUI и SnapKit.  В качестве архитектуры проекта выбирал MVC и MVP.  Для базы данных использовал Realm и Core Data.  Подключал зависимости через Swift Package Manager и CocoaPods")
             TextField("send me a message", text: $message)
                 .frame(height: 35)
                 .padding(.leading)
